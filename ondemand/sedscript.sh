0a74db0f8f8c90f7d6881bd9ab1927db19e4a95e
Michael Collins <98608611+MC965348@users.noreply.github.com>
2023-02-22 13:54:32 -0500

    Update sedscript.sh

--- ondemand/sedscript.sh
+++ ondemand/sedscript.sh
@@ -3,4 +3,4 @@
 set -x 
 export KUBEHEAD=$(hostname -f)
 #sed -i "s/localhost/$(hostname -f)/g" install.sh
-sed -i "s/localhost/$(KUBEHEAD)/g" install.sh
+sed -i "s/localhost/${KUBEHEAD}/g" install.sh
